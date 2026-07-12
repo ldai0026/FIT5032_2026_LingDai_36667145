9b6cd30f5cff039de0c6207340f2d11d00e449a2
Administrator <ldai0026@student.monash.edu>
2026-07-13 00:03:52 +0800

    Add week 4 form validation and PrimeVue table

--- ldai-library/src/components/Form.vue
+++ ldai-library/src/components/Form.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref } from 'vue'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
 
 const formData = ref({
   username: '',
@@ -11,8 +13,80 @@ const formData = ref({
 
 const submittedCards = ref([])
 
+const errors = ref({
+  username: null,
+  password: null,
+  resident: null,
+  gender: null,
+  reason: null,
+})
+
+const validateName = (blur) => {
+  if (formData.value.username.length < 3) {
+    if (blur) errors.value.username = 'Name must be at least 3 characters'
+  } else {
+    errors.value.username = null
+  }
+}
+
+const validatePassword = (blur) => {
+  const password = formData.value.password
+  const minLength = 8
+  const hasUppercase = /[A-Z]/.test(password)
+  const hasLowercase = /[a-z]/.test(password)
+  const hasNumber = /\d/.test(password)
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password)
+
+  if (password.length < minLength) {
+    if (blur) errors.value.password = `Password must be at least ${minLength} characters long.`
+  } else if (!hasUppercase) {
+    if (blur) errors.value.password = 'Password must contain at least one uppercase letter.'
+  } else if (!hasLowercase) {
+    if (blur) errors.value.password = 'Password must contain at least one lowercase letter.'
+  } else if (!hasNumber) {
+    if (blur) errors.value.password = 'Password must contain at least one number.'
+  } else if (!hasSpecialChar) {
+    if (blur) errors.value.password = 'Password must contain at least one special character.'
+  } else {
+    errors.value.password = null
+  }
+}
+
+const validateResident = (blur) => {
+  if (!formData.value.isAustralian) {
+    if (blur) errors.value.resident = 'Australian residency confirmation is required'
+  } else {
+    errors.value.resident = null
+  }
+}
+
+const validateGender = (blur) => {
+  if (!formData.value.gender) {
+    if (blur) errors.value.gender = 'Please select a gender'
+  } else {
+    errors.value.gender = null
+  }
+}
+
+const validateReason = (blur) => {
+  if (formData.value.reason.trim().length < 10) {
+    if (blur) errors.value.reason = 'Reason must be at least 10 characters'
+  } else {
+    errors.value.reason = null
+  }
+}
+
 const submitForm = () => {
-  submittedCards.value.push({ ...formData.value })
+  validateName(true)
+  validatePassword(true)
+  validateResident(true)
+  validateGender(true)
+  validateReason(true)
+
+  if (Object.values(errors.value).every((error) => !error)) {
+    submittedCards.value.push({ ...formData.value })
+    clearForm()
+  }
 }
 
 const clearForm = () => {
@@ -22,6 +96,14 @@ const clearForm = () => {
     isAustralian: false,
     reason: '',
     gender: '',
+  }
+
+  errors.value = {
+    username: null,
+    password: null,
+    resident: null,
+    gender: null,
+    reason: null,
   }
 }
 </script>
@@ -35,11 +117,27 @@ const clearForm = () => {
           <div class="row mb-3">
             <div class="col-12 col-sm-6">
               <label for="username" class="form-label">Username</label>
-              <input id="username" v-model="formData.username" type="text" class="form-control" />
+              <input
+                id="username"
+                v-model="formData.username"
+                type="text"
+                class="form-control"
+                @blur="() => validateName(true)"
+                @input="() => validateName(false)"
+              />
+              <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
             </div>
             <div class="col-12 col-sm-6">
               <label for="password" class="form-label">Password</label>
-              <input id="password" v-model="formData.password" type="password" class="form-control" />
+              <input
+                id="password"
+                v-model="formData.password"
+                type="password"
+                class="form-control"
+                @blur="() => validatePassword(true)"
+                @input="() => validatePassword(false)"
+              />
+              <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
             </div>
           </div>
 
@@ -51,23 +149,42 @@ const clearForm = () => {
                   v-model="formData.isAustralian"
                   type="checkbox"
                   class="form-check-input"
+                  @blur="() => validateResident(true)"
+                  @change="() => validateResident(false)"
                 />
                 <label class="form-check-label" for="isAustralian">Australian Resident?</label>
+                <div v-if="errors.resident" class="text-danger">{{ errors.resident }}</div>
               </div>
             </div>
             <div class="col-12 col-sm-6">
               <label for="gender" class="form-label">Gender</label>
-              <select id="gender" v-model="formData.gender" class="form-select">
+              <select
+                id="gender"
+                v-model="formData.gender"
+                class="form-select"
+                @blur="() => validateGender(true)"
+                @change="() => validateGender(false)"
+              >
+                <option disabled value="">Please select</option>
                 <option value="male">Male</option>
                 <option value="female">Female</option>
                 <option value="other">Other</option>
               </select>
+              <div v-if="errors.gender" class="text-danger">{{ errors.gender }}</div>
             </div>
           </div>
 
           <div class="mb-3">
             <label for="reason" class="form-label">Reason for joining</label>
-            <textarea id="reason" v-model="formData.reason" class="form-control" rows="3"></textarea>
+            <textarea
+              id="reason"
+              v-model="formData.reason"
+              class="form-control"
+              rows="3"
+              @blur="() => validateReason(true)"
+              @input="() => validateReason(false)"
+            ></textarea>
+            <div v-if="errors.reason" class="text-danger">{{ errors.reason }}</div>
           </div>
 
           <div class="text-center">
@@ -76,47 +193,18 @@ const clearForm = () => {
           </div>
         </form>
 
-        <div class="row mt-5" v-if="submittedCards.length">
-          <div class="d-flex flex-wrap justify-content-start">
-            <div
-              v-for="(card, index) in submittedCards"
-              :key="index"
-              class="card m-2"
-              style="width: 18rem"
-            >
-              <div class="card-header">User Information</div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Username: {{ card.username }}</li>
-                <li class="list-group-item">Password: {{ card.password }}</li>
-                <li class="list-group-item">
-                  Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}
-                </li>
-                <li class="list-group-item">Gender: {{ card.gender }}</li>
-                <li class="list-group-item">Reason: {{ card.reason }}</li>
-              </ul>
-            </div>
-          </div>
+        <div v-if="submittedCards.length" class="mt-5">
+          <DataTable :value="submittedCards" tableStyle="table-layout: fixed; width: 100%">
+            <Column field="username" header="Username"></Column>
+            <Column field="password" header="Password"></Column>
+            <Column header="Australian Resident">
+              <template #body="{ data }">{{ data.isAustralian ? 'Yes' : 'No' }}</template>
+            </Column>
+            <Column field="gender" header="Gender"></Column>
+            <Column field="reason" header="Reason"></Column>
+          </DataTable>
         </div>
       </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-.card {
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-
-.card-header {
-  background-color: #275FDA;
-  color: white;
-  padding: 10px;
-  border-radius: 10px 10px 0 0;
-}
-
-.list-group-item {
-  padding: 10px;
-}
-</style>
